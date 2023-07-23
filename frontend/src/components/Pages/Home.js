3ed0a21ac45b96233db9801d1a8e9b8fb1095a09
djd <dananjayahbi@gmail.com>
2023-07-23 14:00:48 +0530

    fixed some issues

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "../../styles/home.css";
-
+import ButtonWrapper from "../FormsUI/Button";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import LocallyFilesUpload from "../Pages/LocallyFilesUpload";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleLinkClick = () => {
+    navigate("/uploadFiles");
+  };
 
   return (
-    <div>
-      <h1>Dashboard content</h1>
-    </div>
+    <>
+      <div>
+        <h1>Dashboard content</h1>
+        <ButtonWrapper variant="contained" onClick={handleLinkClick}>
+          A DropZone for Upload files in to a designated folder inside the project (inside the backend)
+        </ButtonWrapper>
+        <Routes>
+          <Route exact path="/uploadFiles" element={<LocallyFilesUpload />} />
+        </Routes>
+      </div>
+    </>
   );
 }
